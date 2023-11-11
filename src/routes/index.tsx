7eab7fdab12b841b882fec089188af1b1f6bc6ae
Faruk Aydin <farukaydin@outlook.com>
2023-11-11 20:34:34 +0300

    simplify code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createQuery } from "@tanstack/solid-query";
 import { gql, request } from "graphql-request";
+import { Suspense, Switch, Match } from "solid-js";
 import { Title } from "solid-start";
-import { QueryBoundary } from "~/components/query-boundary";
 
 const endpoint = "https://graphqlzero.almansi.me/api";
 
@@ -34,14 +34,31 @@ export default function Test() {
 
       <h1>Solid Query - Test</h1>
 
-      <QueryBoundary
-        loadingFallback={"loading..."}
-        notFoundFallback={"not found"}
-        query={query}
-        errorFallback={"error"}
-      >
-        {(data) => <pre>{JSON.stringify(data, null, 2)}</pre>}
-      </QueryBoundary>
+      {/* // The commented code below works */}
+      {/* <Suspense fallback={"loading"}>
+        <pre>
+          <code>{JSON.stringify(query.data, null, 2)}</code>
+        </pre>
+      </Suspense> */}
+
+      {/* // The code below does not work, double rendering bug happens in switch match */}
+      <Suspense fallback={"loading"}>
+        <Switch>
+          <Match when={query.isError}>
+            <div>{query.error?.message}</div>
+          </Match>
+
+          <Match when={!query.isFetching && !query.data}>
+            <div>not found</div>
+          </Match>
+
+          <Match when={query.data}>
+            <pre>
+              <code>{JSON.stringify(query.data, null, 2)}</code>
+            </pre>
+          </Match>
+        </Switch>
+      </Suspense>
     </main>
   );
 }
